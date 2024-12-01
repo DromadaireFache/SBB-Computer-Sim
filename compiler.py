ffd5660d654daf4cb2751497da87457c86097049
Charles Benoit <charlesbenoit2005@icloud.com>
2024-12-01 10:52:42 -0500

    parser update

--- compiler.py
+++ compiler.py
@@ -20,6 +20,9 @@ EXPR        = enum()
 DECL        = enum()
 NEW_SCOPE   = enum()
 CALL        = enum()
+ARG         = enum()
+END_OF_ARGS = enum()
+SET_SIZE    = enum()
 
 TOKEN_TYPE_STR = {
     PROGRAM     : "PROGRAM",
@@ -34,9 +37,9 @@ TOKEN_TYPE_STR = {
 
 def throw_error(line_nb: int, msg: str, line: str, ind: int | None = None):
     print(f"[Line {line_nb+1}] {msg}")
-    print(f"{line_nb+1}: {line}")
+    print(f"[{line_nb+1}:{ind+1}] {line}")
     if ind != None:
-        print('^~~'.rjust(ind + len(str(line_nb+1)) + 5))
+        print('^~~'.rjust(ind + len(str(line_nb+1)) + len(str(ind+1)) + 7))
     exit()
 
 def lexer(program: str) -> list[tuple[str, str|int]]:
@@ -168,14 +171,15 @@ GRAMMAR = {
         (STR_LIT,)
     ],
     STATEMENT: [
-        ('var', DECL, IDENTIFIER, ';'),
-        (CALL, IDENTIFIER, '(', ')'),
+        ('var', '[', SET_SIZE, INT_LIT, ']', DECL, IDENTIFIER, ';'),
+        ('var', SET_SIZE, DECL, IDENTIFIER, ';'),
+        (CALL, IDENTIFIER, '(', (ARG, IDENTIFIER, ','), END_OF_ARGS, ')'),
         ('return', EXPR, ';'),
         ('{', (STATEMENT,), '}'),
         (';',)
     ],
     FUNCTION: [
-        ('func', DECL, CALL, IDENTIFIER, NEW_SCOPE, '(', (DECL, IDENTIFIER, ','), ')', STATEMENT)
+        ('func', DECL, CALL, IDENTIFIER, NEW_SCOPE, '(', (DECL, ARG, IDENTIFIER, ','), ')', STATEMENT)
     ],
 }
 
@@ -187,39 +191,111 @@ def parser(program: str, tokens: list[tuple[str, str|int]]) -> list:
     syntax_tree = []
     token_index = 0
 
+    def add_func_arg(scope: dict, var_size: int):
+        for i in range(len(scope.keys())-1, -1, -1):
+            var = list(scope.keys())[i]
+            if scope[var][0] != None:
+                scope[var][0].append(var_size)
+                return
+    
+    def count_real_tokens(tokens):
+        count = 0
+        for token in tokens:
+            if type(token) == str or token in GRAMMAR:
+                count += 1
+        return count
+
     def make(grammar: list,
              syntax_tree: list,
              compound=False,
              decl=False,
              scope:dict={},
-             call=False) -> bool:
+             call=False,
+             arg=False,
+             set_size=False,
+             md=['',0,1]) -> bool:
+        #md: function call name, function argument counter, var size
         nonlocal token_index
         if type(grammar) == int:
             if grammar == tokens[token_index][1]:
                 syntax_tree.append(tokens[token_index])
                 if grammar == IDENTIFIER:
+                    var_name = tokens[token_index][0]
+                    bol = tokens[token_index][2]
+                    i = tokens[token_index][3]
+                    next_line = program.find('\n', bol)
+                    # print(var_name, decl, set_size)
                     if decl:
-                        scope[tokens[token_index][0]] = 1 if call else 0
-                        print(scope)
-                    elif tokens[token_index][0] not in scope:
+                        print('declaration:', var_name, '| size:', md[2], '| size size?:', set_size)
+                        # if set_size:
+                        #     md[2] = 1
+                        scope[var_name] = [[], md[2]] if call else [None, md[2]]
+                        if arg:
+                            add_func_arg(scope, md[2])
+                        # print(scope)
+                    elif var_name not in scope:
+                        throw_error(
+                            line_nb= program.count('\n', 0, bol),
+                            msg= f"Name error; out of scope '{var_name}'",
+                            line= program[bol:next_line],
+                            ind= i-bol-len(var_name),
+                        )
+                    elif call and scope[var_name][0] == None:
+                        throw_error(
+                            line_nb= program.count('\n', 0, bol),
+                            msg= f"Type error; uncallable '{var_name}'",
+                            line= program[bol:next_line],
+                            ind= i-bol-len(var_name),
+                        )
+                    if not decl:
+                        if call:
+                            md[0] = tokens[token_index][0]
+                            md[1] = 0
+                        elif arg:
+                            if len(scope[md[0]][0]) <= md[1]:
+                                throw_error(
+                                    line_nb= program.count('\n', 0, bol),
+                                    msg= f"Argument error; in '{md[0]}' call, "\
+                                         f"argument '{var_name}' not expected",
+                                    line= program[bol:next_line],
+                                    ind= i-bol-len(var_name),
+                                )
+                            if scope[md[0]][0][md[1]] != scope[var_name][1]:
+                                throw_error(
+                                    line_nb= program.count('\n', 0, bol),
+                                    msg= f"Type error; in '{md[0]}' call, "\
+                                         f"incorrect argument type of '{var_name}'\n"\
+                                         f"Expected {scope[md[0]][0][md[1]]} "\
+                                         f"byte(s), instead got {scope[var_name][1]} byte(s)",
+                                    line= program[bol:next_line],
+                                    ind= i-bol-len(var_name),
+                                )
+                            md[1] += 1
+                elif grammar == INT_LIT and set_size:
+                    print('set size at:', tokens[token_index][0])
+                    md[2] = int(tokens[token_index][0])
+                    if md[2] < 1:
                         bol = tokens[token_index][2]
                         i = tokens[token_index][3]
                         next_line = program.find('\n', bol)
                         throw_error(
                             line_nb= program.count('\n', 0, bol),
-                            msg= f"Name error; out of scope '{tokens[token_index][0]}'",
+                            msg= f"Declaration error; zero size variable declaration '{md[2]}'",
                             line= program[bol:next_line],
-                            ind= i-bol-len(tokens[token_index][0]),
+                            ind= i-bol-len(str(md[2])),
                         )
                 token_index += 1
                 return True
             else:
                 return False
-        
+            
+        root_index = token_index
+        # print('grammar', grammar, '| root token:', tokens[root_index][0])
         for variation in grammar:
             valid = True
-            decl = False
-            call = False
+            decl = call = arg = set_size = False
+            # print('variation:', variation, '| looked at token:', tokens[token_index][0])
+            token_index = root_index
             for token in variation:
                 temp_tree = []
                 if token == NEW_SCOPE:
@@ -228,17 +304,34 @@ def parser(program: str, tokens: list[tuple[str, str|int]]) -> list:
                     decl = True
                 elif token == CALL:
                     call = True
+                elif token == ARG:
+                    arg = True
+                elif token == END_OF_ARGS:
+                    if len(scope[md[0]][0]) != md[1]:
+                        bol = tokens[token_index][2]
+                        i = tokens[token_index][3]
+                        next_line = program.find('\n', bol)
+                        throw_error(
+                            line_nb= program.count('\n', 0, bol),
+                            msg= f"Arugment error; in '{md[0]}' call, "\
+                                 f"too few arguments",
+                            line= program[bol:next_line],
+                            ind= i-bol-len(tokens[token_index][0]),
+                        )
+                elif token == SET_SIZE:
+                    md[2] = 1
+                    set_size = True
                 elif type(token) == tuple:
-                    while make([token], temp_tree, True, decl, scope, call):
+                    while make([token], temp_tree, True, decl, scope, call, arg, set_size):
                         syntax_tree.extend(temp_tree)
                         temp_tree = []
-                    if len(temp_tree) == len(token) - 1:
+                    if len(temp_tree) == count_real_tokens(token) - 1:
                         syntax_tree.extend(temp_tree)
                 elif type(token) == str and token == tokens[token_index][1]:
                     syntax_tree.append(tokens[token_index])
                     token_index += 1
                 elif type(token) == int and \
-                    make(GRAMMAR[token], temp_tree, compound, decl, scope, call):
+                    make(GRAMMAR[token], temp_tree, compound, decl, scope, call, arg, set_size):
                     if len(temp_tree) == 1 and token == temp_tree[0][1]:
                         syntax_tree.append(temp_tree[0])
                     else:
@@ -279,9 +372,6 @@ def print_parsed_code(syntax_tree: list[tuple], indent=0) -> None:
                 print(' ' * indent + TOKEN_TYPE_STR[branch[1]] + ': ' + str(branch[0]))
             except KeyError:
                 print(' ' * indent + f"'{branch[1]}': " + str(branch[0]))
-
-def semantic():
-    pass
 
 def optimise():
     pass
